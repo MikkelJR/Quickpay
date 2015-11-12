6324d6930c4bf2ad912468ce50d4cc9d948f2e9a
Mikkel Refsgaard <mikkel.j.refsgaard@gmail.com>
2015-11-12 14:40:17 +0100

    Changed UrlSegment to QueryString

--- QuickPayApi/QuickPayApi/ApiCalls/Merchant/Activity.cs
+++ QuickPayApi/QuickPayApi/ApiCalls/Merchant/Activity.cs
@@ -18,14 +18,14 @@ namespace QuickPayApi.ApiCalls.Merchant
             var client = ProcessRequest.CreateRestClient();
             var request = ProcessRequest.CreateRequest(Method.GET, "/activity");
 
-            if (UserId != null) request.AddParameter("user_id", UserId, ParameterType.UrlSegment);
-            if (TargetType != null) request.AddParameter("target_type", TargetType, ParameterType.UrlSegment);
-            if (TargetId != null) request.AddParameter("target_id", TargetId, ParameterType.UrlSegment);
-            if (Support) request.AddParameter("support", Support, ParameterType.UrlSegment);
-            if (Page != null) request.AddParameter("", UserId, ParameterType.UrlSegment);
-            if (PageSize != null) request.AddParameter("", UserId, ParameterType.UrlSegment);
-            if (SortBy != null) request.AddParameter("", UserId, ParameterType.UrlSegment);
-            if (SortDir != null) request.AddParameter("", UserId, ParameterType.UrlSegment);
+            if (UserId != null) request.AddParameter("user_id", UserId, ParameterType.QueryString);
+            if (TargetType != null) request.AddParameter("target_type", TargetType, ParameterType.QueryString);
+            if (TargetId != null) request.AddParameter("target_id", TargetId, ParameterType.QueryString);
+            if (Support) request.AddParameter("support", Support, ParameterType.QueryString);
+            if (Page != null) request.AddParameter("", UserId, ParameterType.QueryString);
+            if (PageSize != null) request.AddParameter("", UserId, ParameterType.QueryString);
+            if (SortBy != null) request.AddParameter("", UserId, ParameterType.QueryString);
+            if (SortDir != null) request.AddParameter("", UserId, ParameterType.QueryString);
 
             var response = client.Execute(request);
 
--- QuickPayApi/QuickPayApi/ApiCalls/Merchant/Payments.cs
+++ QuickPayApi/QuickPayApi/ApiCalls/Merchant/Payments.cs
@@ -23,19 +23,19 @@ namespace QuickPayApi.ApiCalls.Merchant
             var client = ProcessRequest.CreateRestClient();
             var request = ProcessRequest.CreateRequest(Method.GET, "/payments");
 
-            if (DateYear != null) request.AddParameter("date[year]", DateYear, ParameterType.UrlSegment);
-            if (DateMonth != null) request.AddParameter("date[month]", DateMonth, ParameterType.UrlSegment);
-            if (DateDay != null) request.AddParameter("date[day]", DateDay, ParameterType.UrlSegment);
-            if (DateHour != null) request.AddParameter("date[hour]", DateHour, ParameterType.UrlSegment);
-            if (DateMinute != null) request.AddParameter("date[minute]", DateMinute, ParameterType.UrlSegment);
-            if (Accepted) request.AddParameter("accepted", Accepted, ParameterType.UrlSegment);
-            if (OrderId != null) request.AddParameter("order_id", OrderId, ParameterType.UrlSegment);
-            if (State != null) request.AddParameter("state", State, ParameterType.UrlSegment);
-            if (Id != null) request.AddParameter("id", Id, ParameterType.UrlSegment);
-            if (Page != null) request.AddParameter("page", Page, ParameterType.UrlSegment);
-            if (PageSize != null) request.AddParameter("page_size", PageSize, ParameterType.UrlSegment);
-            if (SortBy != null) request.AddParameter("sort_by", SortBy, ParameterType.UrlSegment);
-            if (SortDir != null) request.AddParameter("sort_dir", SortDir, ParameterType.UrlSegment);
+            if (DateYear != null) request.AddParameter("date[year]", DateYear, ParameterType.QueryString);
+            if (DateMonth != null) request.AddParameter("date[month]", DateMonth, ParameterType.QueryString);
+            if (DateDay != null) request.AddParameter("date[day]", DateDay, ParameterType.QueryString);
+            if (DateHour != null) request.AddParameter("date[hour]", DateHour, ParameterType.QueryString);
+            if (DateMinute != null) request.AddParameter("date[minute]", DateMinute, ParameterType.QueryString);
+            if (Accepted) request.AddParameter("accepted", Accepted, ParameterType.QueryString);
+            if (OrderId != null) request.AddParameter("order_id", OrderId, ParameterType.QueryString);
+            if (State != null) request.AddParameter("state", State, ParameterType.QueryString);
+            if (Id != null) request.AddParameter("id", Id, ParameterType.QueryString);
+            if (Page != null) request.AddParameter("page", Page, ParameterType.QueryString);
+            if (PageSize != null) request.AddParameter("page_size", PageSize, ParameterType.QueryString);
+            if (SortBy != null) request.AddParameter("sort_by", SortBy, ParameterType.QueryString);
+            if (SortDir != null) request.AddParameter("sort_dir", SortDir, ParameterType.QueryString);
 
 
             var response = client.Execute(request);
